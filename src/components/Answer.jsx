7594b5121fdddaff5901fb3a12af804e7beda8b2
izzett222 <iizzeddin62@gmail.com>
2023-03-05 14:41:36 +0200

    remove the Ans component and format classname object

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,28 +1,3 @@
-export function Ans({ correct, text, click, picked, check }) {
-  return check ? (
-    <button
-      disabled={check}
-      className={`border   ${
-        picked && correct
-          ? "bg-[#94D7A2]"
-          : picked && !correct
-          ? "bg-[#F8BCBC]"
-          : "border-[#4d5b9e7e] text-[#29326477]"
-      } px-5 py-1 rounded-[8px] font-medium text-xs`}
-    >
-      {text}
-    </button>
-  ) : (
-    <button
-      onClick={() => click(text)}
-      className={`border border-[#4D5B9E] ${
-        picked && "bg-[#D6DBF5]"
-      } px-5 py-1 rounded-[8px] font-medium text-xs`}
-    >
-      {text}
-    </button>
-  );
-}
 export default function Answer({ styles, disabled, children, click }) {
   return (
     <button
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -6,33 +6,30 @@ export default function Question({ question, check, answerQuestion }) {
     <div>
       <h1 className="font-bold text-[#293264]">{question.question}</h1>
       <div className="flex gap-2 flex-wrap mt-1">
-        {question.randomAnswers.map((el, i) => {
-          const picked = question.chosenAnswer === el;
-          const correct = el === question["correct_answer"];
-          const classes = classNames(
-            {
-              "bg-[#94D7A2]": check && correct,
-            },
-            { "bg-[#F8BCBC]": check && (picked && !correct) },
-            {
-              "border-[#4d5b9e7e] text-[#29326477]":
-                check && !picked && !correct,
-            },
-            { "border-[#4D5B9E] ": !check && !picked },
-            { "border-[#4D5B9E] bg-[#D6DBF5]": !check && picked }
-          );
+        {question.randomAnswers.map((element, index) => {
+          const picked = question.chosenAnswer === element;
+          const correct = element === question["correct_answer"];
+          const classes = classNames({
+            "bg-[#94D7A2]": check && correct,
+            "bg-[#F8BCBC]": check && picked && !correct,
+            "border-[#4d5b9e7e] text-[#29326477]": check && !picked && !correct,
+            "border-[#4D5B9E] ": !check && !picked,
+            "border-[#4D5B9E] bg-[#D6DBF5]": !check && picked,
+          });
           return (
             <Answer
-              key={i}
+              key={index}
               disabled={check}
               click={answerQuestion}
               styles={classes}
             >
-              {el}
+              {element}
             </Answer>
           );
         })}
-        {check && !question.chosenAnswer && <div className="text-[#8f3c3c] font-medium">not answer</div>}
+        {check && !question.chosenAnswer && (
+          <div className="text-[#8f3c3c] font-medium">not answer</div>
+        )}
       </div>
     </div>
   );
